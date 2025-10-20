1d019a78d14c79f576ece9108bc0cc05b3ab7d73
Kosy Anyanwu <kosyfrances@gmail.com>
2025-10-20 10:23:22 +0200

    Improve password form errors
    Co-authored-by: Chibuike Anyanwu <anyanwuchib@gmail.com>

--- users/views.py
+++ users/views.py
@@ -1,6 +1,19 @@
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.shortcuts import redirect, render
+from django import forms
+from django.contrib.auth.models import User
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Customise error messages
+        self.error_messages['password_mismatch'] = 'Passwords do not match. Please try again.'
 
 def index(request):
     # Redirect authenticated users to dashboard
@@ -32,12 +45,12 @@ def signup(request):
         return redirect('dashboard')
 
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect("dashboard")
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, "users/signup.html", {"form": form})
 
